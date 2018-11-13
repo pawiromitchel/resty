4e474ffd761d4a2895b99dfb0176e6b4c3ec4c9f
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2018-11-13 18:42:04 -0300

    added auto loader

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const fs = require("fs");
 
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Require our routes into the application.
-require('./server/routes')(app);
+// Auto load all our routes into the application.
+const normalizedPath = path.join(__dirname, "./server/routes");
+fs.readdirSync(normalizedPath).forEach(function(file) {
+  require("./server/routes/" + file)(app);
+});
 
 // When a route doesnt exist, show the requester this message
 app.get('*', (req, res) => res.status(200).send({
