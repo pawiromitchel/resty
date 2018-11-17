1970388b3a33c31dfe1eb2099c3533c6887a9f4d
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2018-11-17 20:22:38 -0300

    generated by sequelize-auto

--- server/models/roles.js
+++ server/models/roles.js
@@ -1,17 +1,29 @@
-module.exports = (sequelize, DataTypes) => {
+/* jshint indent: 2 */
+
+module.exports = function (sequelize, DataTypes) {
   const Roles = sequelize.define('roles', {
     role_id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
-    name: DataTypes.STRING,
-  }, {});
-  Roles.associate = function(models) {
+    name: {
+      type: DataTypes.STRING(45),
+      allowNull: true,
+      unique: true
+    }
+  }, {
+      tableName: 'roles'
+    }
+  );
+
+  Roles.associate = function (models) {
     // associations can be defined here
     Roles.hasOne(models.users, {
       foreignKey: 'role_id'
     });
   };
+
   return Roles;
 };
--- server/models/users.js
+++ server/models/users.js
@@ -1,21 +1,49 @@
-module.exports = (sequelize, DataTypes) => {
+/* jshint indent: 2 */
+
+module.exports = function (sequelize, DataTypes) {
   const Users = sequelize.define('users', {
     user_id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
-    role_id: DataTypes.INTEGER,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    date_created: DataTypes.DATE,
-    date_updated: DataTypes.DATE,
-  }, {});
-  Users.associate = function(models) {
+    username: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING(1024),
+      allowNull: false
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    role_id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      references: {
+        model: 'roles',
+        key: 'role_id'
+      }
+    },
+    date_updated: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
+      tableName: 'users'
+    }
+  );
+
+  Users.associate = function (models) {
     // associations can be defined here
     Users.belongsTo(models.roles, {
       foreignKey: 'role_id'
     });
   };
+  
   return Users;
 };
